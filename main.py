b0bc30f3d42844ae778c9d5154499d15512b2e85
20338 <>
2021-06-08 02:53:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""This code was made by Nathan Brown. First writen text at 2:36 8/06/2021. Version: 1"""
+def name (Question) :
+  valid = False
+  while not valid :
+
+    respond_with = input (Question)
+    if respond_with == (Question) :
+    respond_with  = "Hello"
+
+
+
+print("Hello welcome to the school canteen whats youre name?")
